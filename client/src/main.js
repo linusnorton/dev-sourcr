10203dd2b0eea507750db3295516938847b2f2ec
Linus Norton <linusnorton@gmail.com>
2014-06-06 22:14:19 +0100

    added fixed nav bar

--- client/src/main.js
+++ client/src/main.js
@@ -1,78 +1,116 @@
-$(function() {	
-	fun.import({ under: window });
+$(function() {  
+    fun.import({ under: window });
 
-	var sourcr = new Sourcr();
+    var sourcr = new Sourcr();
 
-	sourcr.fetch();
+    sourcr.fetch();
 
-	window.onscroll = function(ev) {
-	    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-	        sourcr.fetch();
-	    }
-	};
+    window.onscroll = function(ev) {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            sourcr.fetch();
+        }
+    };
 
-	$('[data-role=search]').click(sourcr.reset);
+    $('[role=search]').submit(sourcr.reset);
+        
 });
 
 
 function Sourcr () {
-	/**
-	 * Vars
-	 */
-	var $table = $('[data-role=results]'),
-		$language = $('[name=language]'),
-		$location = $('[name=location]'),
-		api = 'https://api.github.com/search/users',
-		token = '&access_token=' + window.githubToken,
-		sort = 'followers',
-		num = '10',
-		page = 1,
-		rowTemplateSource = $("#person-placeholder").html(),
-		detailsTemplateSource = $("#person-details").html();
+    /**
+     * Vars
+     */
+    var $table = $('[data-role=results]'),
+        $form = $('[role=search]'),
+        $language = $form.find('[name=language]'),
+        $location = $form.find('[name=location]'),
+        api = 'https://api.github.com/search/users',
+        token = '&access_token=' + window.githubToken,
+        sort = 'followers',
+        num = '10',
+        page = 1,
+        rowTemplateSource = $("#person-placeholder").html(),
+        detailsTemplateSource = $("#person-details").html(),
+        results = {};
+
+    $('[role=export]').submit(function () {
+
+        var data = [["name", "username", "email"].join(',')];
+
+        for (var i in results) {
+            var result = results[i];
+            data.push([result.name, result.login, result.email + ''].join(','));
+        }
+
+        var csvContent = "data:text/csv;charset=utf-8," + data.join("\n");
+        
+        
+        var encodedUri = encodeURI(csvContent);
+        window.open(encodedUri);
+
+        return false;
+    }); 
 
 
-	/**
-	 * Private Functions
-	 */
-	var renderTable = $table.append.bind($table),
-		concat = curry(join, ''),
-		rowTemplate = Handlebars.compile(rowTemplateSource),
-		personTemplate = Handlebars.compile(detailsTemplateSource),
-		buildRows = curry(map, rowTemplate),
-		getItems = pluck('items'),
-		renderResults = compose(renderTable, concat, buildRows, getItems),
-		fetchPeopleDetails = curry(forEach, fetchPersonDetails),
-		fetchMoreDetails = compose(fetchPeopleDetails, getItems);
 
-	function fetchPersonDetails (person) {
-		$.get('https://api.github.com/users/' + person.login + '?' + token, function (data) {
-			$table.find('[data-id=' + person.login + ']').html(personTemplate(data));
-		});
-	}
+    /**
+     * Private Functions
+     */
+    var renderTable = $table.append.bind($table),
+        concat = curry(join, ''),
+        rowTemplate = Handlebars.compile(rowTemplateSource),
+        personTemplate = Handlebars.compile(detailsTemplateSource),
+        buildRows = curry(map, rowTemplate),
+        getItems = pluck('items'),
+        storeResults = curry(map, storeResult),
+        renderResults = compose(renderTable, concat, buildRows, storeResults, getItems),
+        fetchPeopleDetails = curry(forEach, fetchPersonDetails),
+        fetchMoreDetails = compose(fetchPeopleDetails, getItems);
 
-	function getQuery () {
-		return 'type:user language:' + $language.val() + ' location:' + $location.val();		
-	}
+    function fetchPersonDetails (person) {
+        $.get('https://api.github.com/users/' + person.login + '?' + token, function (data) {
+            if (data.blog && data.blog.indexOf('http') === -1) {
+                data.blog = 'http://' + data.blog;
+            }
 
-	/**
-	 * Public functions
-	 */
-	function fetch () {
-		var url = api + '?q=' + getQuery() + '&sort=' + sort + '&per_page=' + num + '&page=' + page++ + token;
+            $table.find('[data-id=' + person.login + ']').html(personTemplate(data));
+            results[person.login].name = data.name;
+            results[person.login].email = data.email;
+        });
+    }
 
-		$.get(encodeURI(url), function (data) {
-			renderResults(data);
-			fetchMoreDetails(data);
-		});
-	}
+    function storeResult (person) {
+        results[person.login] = person;
 
-	function reset () {
-		page = 1;
-		$table.html('');
-		fetch();
-	}
+        return person;
+    }
 
-	this.fetch = fetch;
-	this.reset = reset;
+    function getQuery () {
+        return 'type:user language:' + $language.val() + ' location:' + $location.val();        
+    }
+
+    /**
+     * Public functions
+     */
+    function fetch () {
+        var url = api + '?q=' + getQuery() + '&sort=' + sort + '&per_page=' + num + '&page=' + page++ + token;
+
+        $.get(encodeURI(url), function (data) {
+            renderResults(data);
+            fetchMoreDetails(data);
+        });
+    }
+
+    function reset () {
+        page = 1;
+        results = {};
+        $table.html('');
+        fetch();
+
+        return false;
+    }
+
+    this.fetch = fetch;
+    this.reset = reset;
 
 }
